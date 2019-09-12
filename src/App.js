ca7e33085c19b520c79ce759f1ccc911d89999bb
Jelte Homminga <jeltehomminga@gmail.com>
2019-09-12 10:29:00 +0200

    fix default export

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { MenuToggle } from './MenuToggle';
+import MenuToggle from './MenuToggle';
 
 
 function App () {
